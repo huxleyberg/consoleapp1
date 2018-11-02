fdaf9e62b90299e3a0182a49b7f81f9cb1430d60
Chidi Udeogu <chidi.udeogu@timwe.com>
2018-11-02 15:31:37 +0100

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace consoleapp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+        public void Configure(IApplicationBuilder app,
+        IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello, world!");
+            });
+        }
+    }
+}
